2e0a0f36b24eb812e179c9d8bff31eac3684338f
Suchita <coralmaven@yahoo.com>
2019-05-01 23:40:14 -0500

    fixed listall

--- static/js/app.js
+++ static/js/app.js
@@ -2,47 +2,36 @@
 // from data.js
 var tableData = data;
 const ufoTable = d3.select("tbody");
-tableData.forEach(ufoSighting => addRowToTable(ufoSighting));
-
-const 
-  dates = tableData.map(t => t["datetime"]),
-  cities = tableData.map(t => t["city"]),
-  states = tableData.map(t => t["state"]),
-  countries = tableData.map(t => t["country"]),
-  shapes = tableData.map(t => t["shape"]);
-
+const keys = ['datetime', 'city', 'state', 'country', 'shape'];
+const keyElem = ['#datetimes', '#cities', '#states', '#countries', '#shapes'];
 
 const unique = (value, index, self) => {
-    return self.indexOf(value) === index;
-  }
-  console.log(dates.filter(unique));
-
+  return self.indexOf(value) === index;
+}
+  
 function addDropDownOptions(elem, arr){
-    for (i=0; i<arr.length; i++)
-      elem.append("option").text(arr[i]).html(arr[i]);
+  for (i=0; i<arr.length; i++){
+    elem.append("option").text(arr[i]).html(arr[i]);
+  }
+  if(arr.length > 1){
+    elem.property("value","");
+  } else {
+    elem.property("value", arr[0]);
+  }
+
 }
 
-
-const keys = ['datetime', 'city', 'state', 'country', 'shape'];
-const keyElem = ['#datetime', '#city', '#state', '#country', '#shape']
-
-addDropDownOptions(d3.select("#datetime"), dates.filter(unique).sort());
-addDropDownOptions(d3.select("#city"), cities.filter(unique).sort());
-addDropDownOptions(d3.select("#state"), states.filter(unique).sort());
-addDropDownOptions(d3.select("#country"), countries.filter(unique).sort());
-addDropDownOptions(d3.select("#shape"), shapes.filter(unique).sort());
-
 function addRowToTable(ufoSighting){
-    const row = ufoTable.append("tr");
-    for (const key in ufoSighting){
-        if(key==="state" || key==="country"){
-            row.append("td").text(ufoSighting[key].toUpperCase());
-        } else if(key==="city" || key==="shape"){
-            row.append("td").text(titleCase(ufoSighting[key]));
-        } else {
-            row.append("td").text(ufoSighting[key]);
-        }
+  const row = ufoTable.append("tr");
+  for (const key in ufoSighting){
+    if(key==="state" || key==="country"){
+        row.append("td").text(ufoSighting[key].toUpperCase());
+    } else if(key==="city" || key==="shape"){
+        row.append("td").text(titleCase(ufoSighting[key]));
+    } else {
+        row.append("td").text(ufoSighting[key]);
     }
+  }
 }
 
 function titleCase(str) {
@@ -54,38 +43,48 @@ function titleCase(str) {
   return str.join(' ');
 };
 
-function filter_by_one_key(keyValue, key){
-  console.log(keyValue, key);
-  if(keyValue!=null){
-    console.log(keyValue);
-    ufoTable.selectAll("tr").remove();
-
-    const filteredData = tableData.filter(ufoSighting => (ufoSighting[key] === keyValue));
-    filteredData.forEach(ufoSighting => addRowToTable(ufoSighting));
-  }
-};
-
 function filter_by(){
   ufoTable.selectAll("tr").remove();
   let filteredData = tableData;
   for (var i = 0; i < keys.length; i++){
     keyVal =d3.select(keyElem[i]).property("value");
-    console.log(keys[i], keyElem[i], keyVal);
     if ( keyVal != ""){
       filteredData = filteredData.filter(ufoSighting => (ufoSighting[keys[i]] === keyVal));
     }
   }
   filteredData.forEach(ufoSighting => addRowToTable(ufoSighting));
+  updateDropDownLists(filteredData);
 };
+
 // Using the UFO dataset provided in the form of an array of JavaScript
 // objects, write code that appends a table to your web page and then 
 // adds new rows of data for each UFO sighting.
 
-d3.select("#list-all").on("click", function(){
-  console.log(tableData);
+
+d3.select("#list-all").on("click",listAll());
+
+function listAll(){
+  d3.selectAll("option").remove();
   tableData.forEach(ufoSighting => addRowToTable(ufoSighting));
   for (var i = 0; i < keys.length; i++){
-    keyVal =d3.select(keyElem[i]).property("value","");
+    updateDropDownLists(tableData);
   }
-});
+};
 
+
+function updateDropDownLists(theTable){
+  d3.selectAll("option").remove();
+const 
+  dates = theTable.map(t => t["datetime"]),
+  cities = theTable.map(t => t["city"]),
+  states = theTable.map(t => t["state"]),
+  countries = theTable.map(t => t["country"]),
+  shapes = theTable.map(t => t["shape"]);
+
+  addDropDownOptions(d3.select("#datetimes"), dates.filter(unique).sort());
+  addDropDownOptions(d3.select("#cities"), cities.filter(unique).sort());
+  addDropDownOptions(d3.select("#states"), states.filter(unique).sort());
+  addDropDownOptions(d3.select("#countries"), countries.filter(unique).sort());
+  addDropDownOptions(d3.select("#shapes"), shapes.filter(unique).sort());
+}
+  
